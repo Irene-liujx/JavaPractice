9d010658050db5241f35bd217b2a9cf29c763d95
Irene_Liu <2410062041@qq.com>
2019-11-25 21:34:19 +0800

    数组示例:数组的定义和引用数组元素

---
+++ ArrayTest.java
@@ -0,0 +1,15 @@
+//定义数组和引用数组元素的示例
+package BookDemo;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int i;
+        int a[] = new int[5];
+        for(i = 0;i < 5;i++){
+            a[i] = i;
+        }
+        for(i = a.length - 1;i >= 0;i--){
+            System.out.println("a[" + i + "]= " + a[i]);
+        }
+    }
+}
