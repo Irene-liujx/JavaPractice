739a6f5ad7096e79a3b01ab698f98640feecfbe5
Irene_Liu <2410062041@qq.com>
2019-12-15 21:51:50 +0800

    一个五位数,判断它是不是回文数

---
+++ text25.java
@@ -0,0 +1,40 @@
+//判断一个5位数是不是回文数
+package Demo40;
+
+import java.util.Scanner;
+
+public class text25 {
+    static int[] a = new int[5];
+    static int[] b = new int[5];
+
+    public static void main(String[] args) {
+        boolean is = false;
+        Scanner s = new Scanner(System.in);
+        long l = s.nextLong();
+        if(l > 99999 || l < 10000){
+            System.out.println("Input error,please input again!");
+            l = s.nextLong();
+        }
+        for(int i = 4;i >= 0;i--){
+            a[i] = (int) (l/(long)Math.pow(10,i));
+            l = (l % (long) Math.pow(10,i));
+        }
+        System.out.println();
+        for(int i = 0,j = 0;i < 5;i++,j++){
+            b[j] = a[i];
+        }
+        for(int i = 0,j = 4;i < 5;i++,j--){
+            if(a[i] != b[j]){
+                is = false;
+                break;
+            }else{
+                is = true;
+            }
+        }
+        if(is == false){
+            System.out.println("不是回文数");
+        }else if(is == true){
+            System.out.println("是回文数");
+        }
+    }
+}
