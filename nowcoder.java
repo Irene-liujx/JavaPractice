684e775d1f2e9a2609a48d6122b5fb99b3f20a13
Irene_Liu <2410062041@qq.com>
2020-04-11 21:49:21 +0800

    牛客网练习:n个数里出现次数大于等于n/2的数

--- nowcoder.java
+++ nowcoder.java
@@ -392,3 +392,34 @@ public class Solution {
  }
  
  
+ 
+//n个数里出现次数大于等于n/2的数
+//str.split(" "):按照空格切分字符串    Integer.valueOf(str):将数字字符串转为int
+ import java.util.*;
+ public class Main {
+	 public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
+		 while (in.hasNext()) { 
+		 String str = in.nextLine(); 
+		 String[] strs = str.split(" "); 
+		 int[] arr = new int[strs.length]; 
+		 for (int i = 0; i < arr.length; i++) { 
+			arr[i] = Integer.valueOf(strs[i]); 
+		 }
+		 int num = arr[0]; 
+		 int count = 0;
+		 for (int j = 1; j < arr.length; j++) { 
+			if (arr[j] == num) { 
+			count++; 
+		 } else if (count > 0) { 
+			count--; 
+		} else { 
+			num = arr[j];
+			} 
+		}
+		System.out.println(num);
+		}
+	} 
+ }
+
+
