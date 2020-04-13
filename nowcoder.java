040a9625581bdadb4d8b4d113850181ccd7dded8
Irene_Liu <2410062041@qq.com>
2020-04-13 21:39:56 +0800

    牛客网练习:求最小公倍数

--- nowcoder.java
+++ nowcoder.java
@@ -442,3 +442,22 @@ public static void main(String[] args) {
 	System.out.println(ans); 
 	} 
 }
+
+//求最小公倍数
+//两个数a,b的最小公倍数是a*b/gcd(a,b)。由于两个数的乘积等于这两个数的最大公约数与最小公倍数的积，即（a，b）× [a，b] = a × b
+//求最大公约数： 1. a/b，令r为所得余数（0≤r<b） 若r=0，算法结束，a即为答案     2. 互换：置 a←b，b←r，并返回第一步
+import java.util.Scanner; 
+public class Main {
+	public static int lcm(int m, int n) { 
+		return (m * n) / gcd(m, n); 
+	}
+private static int gcd(int a, int b) {
+	if (b == 0) return a; return gcd(b, a % b); 
+	}
+public static void main(String[] args) { 
+	Scanner in = new Scanner(System.in);
+	int a = in.nextInt(); 
+	int b = in.nextInt(); 
+	System.out.println(lcm(a, b));
+	}
+ }
