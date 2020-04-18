e2fd17ab24208d55213976f448ce02a302800125
Irene_Liu <2410062041@qq.com>
2020-04-18 21:57:33 +0800

    牛客网练习:个位数统计

--- nowcoder.java
+++ nowcoder.java
@@ -583,3 +583,19 @@ public class Robot {
         return dp[x-1][y-1];
     }
 }
+
+11//个位数统计
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] a = new int[10];
+        String n = in.next();
+        for(int i = 0;i<n.length();i++)
+            a[n.charAt(i)-'0']++;
+        for(int i = 0;i<a.length;i++)
+            if(a[i]!=0)
+                System.out.println(i+":"+a[i]);
+    }
+}
+
