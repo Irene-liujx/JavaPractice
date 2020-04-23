cbdf1010bdbb575dcfc118b136e4856e5df0ecce
Irene_Liu <2410062041@qq.com>
2020-04-23 21:47:46 +0800

    牛客网练习:超长正整数相加

--- nowcoder.java
+++ nowcoder.java
@@ -691,3 +691,18 @@ import java.util.Scanner;
 	 } 
  }
  
+ //超长正整数相加
+ import java.util.*; 
+ import java.math.BigInteger;
+ public class Main{
+	 public static void main(String[] args){ 
+		Scanner input=new Scanner(System.in);
+		while(input.hasNext()){
+			String s1=input.next(); 
+			tring s2=input.next();
+			BigInteger num1=new BigInteger(s1);	//采用BigInteger可以直接进行大整数进行计算
+			BigInteger num2=new BigInteger(s2); 
+			System.out.println(num1.add(num2)); 
+		} 
+	 } 
+ }
