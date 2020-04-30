a4fc0772f993cb70c191b609429e8063480e06e3
Irene_Liu <2410062041@qq.com>
2020-04-30 21:49:04 +0800

    牛客网练习:微信红包

--- nowcoder.java
+++ nowcoder.java
@@ -1080,3 +1080,27 @@ public class Main {
 		} 
 	}
 
+
+20//微信红包
+import java.util.*;
+
+public class Gift {
+    public int getValue(int[] gifts, int n) {
+        // write code here
+       Map<Integer,Integer> map = new TreeMap<>();
+        for(int i = 0;i < n;i++){
+            if(map.containsKey(gifts[i])){
+                map.put(gifts[i],map.get(gifts[i])+1);
+            }else{
+                map.put(gifts[i],1);
+            }
+        }
+        for(int i = 0;i < n;i++){
+            if(map.get(gifts[i]) > n / 2){
+                return gifts[i];
+            }
+        }
+        return 0;
+    }
+}
+
