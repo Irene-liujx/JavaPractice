4cbe83ab2085fd89230dc878bb558d754db0640e
Irene_Liu <2410062041@qq.com>
2020-05-02 13:26:26 +0800

    牛客网练习:统计每个月的兔子总数

--- nowcoder.java
+++ nowcoder.java
@@ -1156,3 +1156,28 @@ public class Partition {
     }
 }
 
+
+21//统计每个月的兔子总数
+
+import java.util.*;
+
+ public class Main{ 
+	public static void main(String[] args){ 
+		Scanner sc=new Scanner(System.in); 
+		while(sc.hasNext()){ 
+			int n=sc.nextInt(); 
+			System.out.println(rabbit(n)); 
+		}
+			sc.close(); 
+	}
+	//递归三要素 1.必须要写一个方法 2.要有出口条件 3.必须有一定的规律 
+	public static int rabbit(int n){ 
+		if(n<=2)
+	//月份小于等于2只有一个兔子 
+		return 1; 
+		else{
+			return rabbit(n-1)+rabbit(n-2); 
+			} 
+	}
+}
+
