ac3792c9c620350af4d243450040925236b76110
Irene_Liu <2410062041@qq.com>
2020-05-11 21:36:37 +0800

    牛客网习题:记票统计

--- nowcoder.java
+++ nowcoder.java
@@ -1698,3 +1698,49 @@ public class Main{
 	} 
 }
 
+
+
+//记票统计
+import java.util.Scanner;
+import java.util.HashMap; 
+import java.util.Set; 
+
+public class Main{ 
+	public static void main(String args[]){ 
+		Scanner in = new Scanner(System.in); 	//控制台输入数据
+		
+		while (in.hasNext()){ 
+			int n = in.nextInt(); //候选人人数 
+			String[] s = new String[n]; 
+			 //记录候选人 
+			HashMap<String,Integer> hs = new HashMap<String,Integer>(); 
+			for(int i=0;i<n;i++){ 
+				String temp = in.next(); 
+				hs.put(temp,0); 
+				s[i] = temp; 
+			}
+			
+			//投票人数 
+			int m = in.nextInt(); 
+			Set<String> keys = hs.keySet(); 
+			int cnt = 0; 
+			for(int i=0;i<m;i++){ 
+				//投票 
+				String tick = in.next(); 
+				if(keys.contains(tick)){ 
+					hs.put(tick, hs.get(tick)+1); 
+				}else{
+					cnt++; 
+				} 
+			}
+			
+			  //输出结果 
+			for(String k:s){ 
+				System.out.println(k+" : "+hs.get(k)); 
+			}
+			System.out.println("Invalid : "+cnt); 
+		}
+		in.close(); 
+	} 
+}
+
