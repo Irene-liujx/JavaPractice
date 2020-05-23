36acbd52d5de20c57dd5b5a9d5955d1585a03371
Irene_Liu <2410062041@qq.com>
2020-05-23 21:16:17 +0800

    牛客网练习:下厨房

--- nowcoder.java
+++ nowcoder.java
@@ -2445,3 +2445,18 @@ public class Main {
 	} 
 }
 
+
+
+35//下厨房
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Set<String> set = new HashSet<String>();
+        while(sc.hasNext()){
+            String s = sc.next();
+            set.add(s);
+        }
+        System.out.println(set.size());
+    }
+}
