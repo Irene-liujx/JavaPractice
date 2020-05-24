6fc04e492d8331a0fda11d9de4012a22432bc22c
Irene_Liu <2410062041@qq.com>
2020-05-24 20:25:53 +0800

    牛客网练习:字母统计

--- nowcoder.java
+++ nowcoder.java
@@ -2460,3 +2460,64 @@ public class Main{
         System.out.println(set.size());
     }
 }
+
+
+
+//百万富翁问题
+import java.lang.Math; 
+
+public class Main{ 
+	public static void main(String[] args){ 
+		long sumRich = 30 * 10; 
+		long sumStranger = (1L << 30) - 1; 
+		System.out.print(sumRich + " " + sumStranger); 
+	} 
+}
+
+
+//风口的猪-中国牛市
+
+public class Solution { 
+	public static int calculateMax(int[] prices) { 
+		int firstBuy = Integer.MAX_VALUE;    // 第一次买入最好的价格，越低越好 
+		int firstSell = 0;                   // 第一次卖出后的最高收益，越高越好 
+		int secondBuy = Integer.MIN_VALUE;   // 第二次买入时还剩余的最高收益，越高越好 
+		int secondSell = 0;                  // 第二次卖出时总的最高收益，越高越好 
+		for (int price : prices) { 
+			// 当前价格下第一次买入的价格 
+			firstBuy = Math.min(firstBuy, price); 
+			// 当前价格 - 买入价格 就是当前价格下第一次买卖的收益 
+			firstSell = Math.max(firstSell, price - firstBuy); 
+			// 第一次卖出的收益 - 当前价格，即当前价格下，第二次买入后还剩余收益 
+			secondBuy = Math.max(secondBuy, firstSell - price); 
+			// 剩余收益 + 当前价格，即当前价格下，第二次买卖的收益
+			secondSell = Math.max(secondSell, secondBuy + price); 
+		}
+		return secondSell; 
+	} 
+}
+
+
+
+36//字母统计
+import java.util.*; 
+
+public class Main { 
+	public static void main(String[] args) { 
+		Scanner scanner = new Scanner(System.in); 
+		while (scanner.hasNextLine()) { 
+			String line = scanner.nextLine(); 
+			int[] count = new int[26]; 	// 因为只有 26 个英文字母，所以只需要 26 长度 
+			for (int i = 0; i < line.length(); i++) { 
+				char ch = line.charAt(i); 
+				if (ch >= 'A' && ch <= 'Z') { 
+					// 哈希中的直接定址法 
+					count[ch - 'A']++; 
+				} 
+			}
+			for (int i = 0; i < 26; i++) { 
+				System.out.printf("%c:%d%n", ('A' + i), count[i]); 
+			} 
+		} 
+	} 
+}
