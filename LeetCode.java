c603d97f7f3cc463bba78bbb7c88510f2d701384
Irene_Liu <2410062041@qq.com>
2020-06-27 23:11:29 +0800

    力扣练习:青蛙跳台阶问题

--- LeetCode.java
+++ LeetCode.java
@@ -15,3 +15,17 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+
+//青蛙跳台阶问题
+class Solution {
+    public int numWays(int n) {
+        int a = 1, b = 1, sum;
+        for(int i = 0; i < n; i++){
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+}
