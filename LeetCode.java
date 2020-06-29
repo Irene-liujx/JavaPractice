31d757832deaaca83aa707abab26e1fa437b03ad
Irene_Liu <2410062041@qq.com>
2020-06-29 22:30:21 +0800

    力扣练习:链表中倒数第K个节点

--- LeetCode.java
+++ LeetCode.java
@@ -29,3 +29,32 @@ class Solution {
         return a;
     }
 }
+
+
+//链表中倒数第K个节点
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+class Solution {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode frontNode = head, behindNode = head;
+		
+        while (frontNode != null && k > 0) {
+            frontNode = frontNode.next;
+            k--;
+        }
+
+        while (frontNode != null) {
+            frontNode = frontNode.next;
+            behindNode = behindNode.next;
+        }
+
+        return behindNode;
+    }
+}
