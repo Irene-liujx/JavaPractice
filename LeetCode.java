413f4e2abe95d3c9bb5132fb6b0e5ac6dc68f5ec
Irene_Liu <2410062041@qq.com>
2020-07-01 22:17:13 +0800

    力扣练习:调整数组顺序使奇数位于偶数前面

--- LeetCode.java
+++ LeetCode.java
@@ -96,3 +96,27 @@ class CQueue {
  * obj.appendTail(value);
  * int param_2 = obj.deleteHead();
  */
+ 
+ 
+ 
+ //调整数组顺序使奇数位于偶数前面
+ class Solution {
+    public int[] exchange(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            while (left < right && nums[left] % 2 != 0) {
+                left++;
+            }
+            while (left < right && nums[right] % 2 == 0) {
+                right--;
+            }
+            if (left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+            }
+        }
+        return nums;
+    }
+}
