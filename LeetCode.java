ab5e52a5907b5afd4e3be94ae93b59916b8ea9a9
Irene_Liu <2410062041@qq.com>
2020-07-02 22:22:16 +0800

    力扣练习:栈的压入、弹出序列

--- LeetCode.java
+++ LeetCode.java
@@ -120,3 +120,20 @@ class CQueue {
         return nums;
     }
 }
+
+
+//栈的压入、弹出序列
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for(int num : pushed) {
+            stack.push(num); 	// num 入栈
+            while(!stack.isEmpty() && stack.peek() == popped[i]){ 
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
