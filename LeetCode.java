91332d811e0f56c5865f022f850c8c1a5bba95c5
Irene_Liu <2410062041@qq.com>
2020-07-05 20:24:49 +0800

    力扣练习:连续子数组的最大和

--- LeetCode.java
+++ LeetCode.java
@@ -209,3 +209,17 @@ class MaxQueue {
  * obj.push_back(value);
  * int param_3 = obj.pop_front();
  */
+ 
+ 
+ 
+ //连续子数组的最大和
+ class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(res, nums[i]);
+        }
+        return res;
+    }
+}
