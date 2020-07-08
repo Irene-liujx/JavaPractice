0cbe246d3824f0119d5bd57299e2bd536ec7a47a
Irene_Liu <2410062041@qq.com>
2020-07-08 22:27:35 +0800

    力扣练习:滑动窗口的最大值

--- LeetCode.java
+++ LeetCode.java
@@ -238,3 +238,32 @@ class Solution {
         return profit;
     }
 }
+
+
+//滑动窗口的最大值
+//单调双向队列
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+         if (nums == null || k < 1 || nums.length < k) {
+            return new int[0];
+        }
+
+        int index = 0;
+        int[] res = new int[nums.length - k + 1];
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            while (!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]) {
+                queue.pollLast();
+            }
+            queue.addLast(i);
+            if (queue.peekFirst() == (i - k)) {
+                queue.pollFirst();
+            }
+            if (i >= (k - 1)) {
+                res[index++] = nums[queue.peekFirst()];
+            }
+        }
+        return res;
+    }
+}
