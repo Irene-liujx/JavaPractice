1436078e830c3c9444a73a11c1e8447fa219f024
Irene_Liu <2410062041@qq.com>
2020-07-16 22:51:35 +0800

    力扣练习:树的子结构

--- LeetCode.java
+++ LeetCode.java
@@ -380,3 +380,41 @@ class Solution {
         return dp[n];
     }
 }
+
+
+
+//树的子结构
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (B == null || A == null) {
+            return false;
+        }
+        if (A.val == B.val && (recur(A.left, B.left) && recur(A.right, B.right))) {
+            return true;
+        }
+        return isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    private boolean recur(TreeNode A, TreeNode B) {
+        if (B == null) {
+            return true;
+        }
+        if (A == null) {
+            return false;
+        }
+        if (A.val == B.val) {
+            return recur(A.left, B.left) && recur(A.right, B.right);
+        } else {
+            return false;
+        }
+    }
+}
