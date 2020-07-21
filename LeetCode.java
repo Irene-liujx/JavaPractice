80907a2c044dd5c635a5d1b64a499acbe894a7b9
Irene_Liu <2410062041@qq.com>
2020-07-21 22:05:07 +0800

    力扣练习:左旋转字符串

--- LeetCode.java
+++ LeetCode.java
@@ -475,3 +475,17 @@ class Solution {
         return map.get(head);
     }
 }
+
+
+
+//左旋转字符串
+class Solution {
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder res = new StringBuilder();
+        for(int i = n; i < s.length(); i++)
+            res.append(s.charAt(i));
+        for(int i = 0; i < n; i++)
+            res.append(s.charAt(i));
+        return res.toString();
+    }
+}
