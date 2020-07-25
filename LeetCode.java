c338c62584416c728b8abe053867e1fe39016807
Irene_Liu <2410062041@qq.com>
2020-07-25 22:56:45 +0800

    力扣练习:剪绳子

--- LeetCode.java
+++ LeetCode.java
@@ -489,3 +489,19 @@ class Solution {
         return res.toString();
     }
 }
+
+
+
+//剪绳子
+class Solution {
+    public int cuttingRope(int n) {
+        if(n <= 3) 
+            return n - 1;
+        int a = n / 3, b = n % 3;
+        if(b == 0)
+            return (int)Math.pow(3, a);
+        if(b == 1) 
+            return (int)Math.pow(3, a - 1) * 4;
+        return (int)Math.pow(3, a) * 2;
+    }
+}
