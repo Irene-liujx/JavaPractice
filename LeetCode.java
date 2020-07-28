4d70c593588e4c6782b1d0d2e8a23b1ed22d36eb
Irene_Liu <2410062041@qq.com>
2020-07-28 22:08:44 +0800

    力扣练习:最长不含重复字符的子字符串

--- LeetCode.java
+++ LeetCode.java
@@ -531,3 +531,20 @@ class Solution {
         return res;
     }
 }
+
+
+
+//最长不含重复字符的子字符串
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int i = -1, res = 0;
+        for(int j = 0; j < s.length(); j++) {
+            if(dic.containsKey(s.charAt(j)))
+                i = Math.max(i, dic.get(s.charAt(j)));
+            dic.put(s.charAt(j), j); 
+            res = Math.max(res, j - i); 
+        }
+        return res;
+    }
+}
