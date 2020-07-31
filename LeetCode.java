18d5fdc5df55d6320eacc6454e9df125d02b45fe
Irene_Liu <2410062041@qq.com>
2020-07-31 23:57:26 +0800

    力扣练习:魔术索引

--- LeetCode.java
+++ LeetCode.java
@@ -562,3 +562,27 @@ class Solution {
         return a;
     }
 }
+
+
+
+//魔术索引
+class Solution {
+    public int findMagicIndex(int[] nums) {
+        return getAnswer(nums, 0, nums.length - 1);
+    }
+
+    public int getAnswer(int[] nums, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (right - left) / 2 + left;
+        int leftAnswer = getAnswer(nums, left, mid - 1);
+        if (leftAnswer != -1) {
+            return leftAnswer;
+        } else if (nums[mid] == mid) {
+            return mid;
+        }
+        return getAnswer(nums, mid + 1, right);
+    }
+}
+
