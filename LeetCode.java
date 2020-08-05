a3cef519bf376c305c59f2c65b99feced7580a77
Irene_Liu <2410062041@qq.com>
2020-08-05 21:58:32 +0800

    力扣练习:把数字翻译成字符串

--- LeetCode.java
+++ LeetCode.java
@@ -676,3 +676,38 @@ class Solution {
         return true;
     }
 }
+
+
+
+//把数字翻译成字符串
+//字符串遍历
+class Solution {
+    public int translateNum(int num) {
+        String s = String.valueOf(num);
+        int a = 1, b = 1;
+        for(int i = 2; i <= s.length(); i++) {
+            String tmp = s.substring(i - 2, i);
+            int c = tmp.compareTo("10") >= 0 && tmp.compareTo("25") <= 0 ? a + b : a;
+            b = a;
+            a = c;
+        }
+        return a;
+    }
+}
+/** 数字求余
+class Solution {
+    public int translateNum(int num) {
+        int a = 1, b = 1, x, y = num % 10;
+        while(num != 0) {
+            num /= 10;
+            x = num % 10;
+            int tmp = 10 * x + y;
+            int c = (tmp >= 10 && tmp <= 25) ? a + b : a;
+            b = a;
+            a = c;
+            y = x;
+        }
+        return a;
+    }
+}
+*/
