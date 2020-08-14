30444e84388bc46982ae7b092c23fddf18a6f933
Irene_Liu <2410062041@qq.com>
2020-08-14 21:55:50 +0800

    力扣练习:数组中数字出现的次数

--- LeetCode.java
+++ LeetCode.java
@@ -1068,3 +1068,32 @@ class Solution {
         return ans.toString();
     }
 }
+
+
+
+//数组中数字出现的次数
+class Solution {
+    public int[] singleNumbers(int[] nums) {
+        int k = 0;
+        
+        for(int num: nums) {
+            k ^= num;
+        }
+
+        int mask = 1;
+        while((k & mask) == 0) {
+            mask <<= 1;
+        }
+        
+        int a = 0;
+        int b = 0;
+        for(int num: nums) {
+            if((num & mask) == 0) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+        return new int[]{a, b};
+    }
+}
