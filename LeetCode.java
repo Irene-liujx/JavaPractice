5154ec36a9649ea7196b2eb3003ade8a81c04680
Irene_Liu <2410062041@qq.com>
2020-08-16 22:30:50 +0800

    力扣练习:图像渲染

--- LeetCode.java
+++ LeetCode.java
@@ -1126,3 +1126,29 @@ class Solution {
         return dp[l][r][k];
     }
 }
+
+
+
+//图像渲染
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int oldColor = image[sr][sc];
+        if(newColor != oldColor)
+            dfs(image, sr, sc, newColor,oldColor);    
+        return image;      
+    }
+
+    public void dfs(int[][] image, int i, int j, int newColor, int oldColor){
+        if(i < 0 || i >= image.length || j < 0 || j >= image[0].length){
+             return;
+        }
+        if(image[i][j] == oldColor){
+            image[i][j] = newColor;
+            dfs(image, i, j - 1, newColor, oldColor);
+            dfs(image, i, j + 1, newColor, oldColor);
+            dfs(image, i - 1, j, newColor, oldColor);
+            dfs(image, i + 1, j, newColor, oldColor);
+        }
+        return; 
+    }
+}
