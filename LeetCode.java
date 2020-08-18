74253fd5e207b1b96667d555d314b57a4639cacf
Irene_Liu <2410062041@qq.com>
2020-08-18 21:59:15 +0800

    力扣练习:平衡二叉树

--- LeetCode.java
+++ LeetCode.java
@@ -1152,3 +1152,31 @@ class Solution {
         return; 
     }
 }
+
+
+//平衡二叉树
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int getHeight(TreeNode root){
+        return root == null ? 0 : Math.max(getHeight(root.left),getHeight(root.right)) + 1;
+    }
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if(Math.abs(left - right) < 2){
+             return isBalanced(root.left) && isBalanced(root.right);
+        }
+       return false;
+    }
+}
