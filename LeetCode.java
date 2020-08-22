a77cd0854edb49cd3e1c680054e3ad0afbf488b6
Irene_Liu <2410062041@qq.com>
2020-08-22 21:57:00 +0800

    力扣练习:反转链表

--- LeetCode.java
+++ LeetCode.java
@@ -1274,3 +1274,27 @@ class Solution {
       return Math.min(minDepth(root.left),minDepth(root.right)) + 1;
     }
 }
+
+
+//反转链表
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+}
